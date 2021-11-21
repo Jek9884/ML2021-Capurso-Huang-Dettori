Implemented gradient descent optimizer Bunch of fixes

function.py
@@ -33,7 +33,7 @@ def squared_loss(exp_val, pred_val):
 
 
 def squared_loss_deriv(exp_val, pred_val):
-    return np.multiply(-1, np.subtract(exp_val, pred_val))
+    return np.subtract(exp_val, pred_val)
 
 
 # Loss function dictionary
@@ -52,10 +52,20 @@ def identity_deriv(x):
     return np.ones(len(x))
 
 
+def sigm(x):
+    return 1 / (1 + np.power(np.e, -x))
+
+
+def sigm_deriv(x):
+    return np.power(np.e, -x) / np.power(1 + np.power(np.e, -x), 2)
+
+
 # Activation function dictionary
 identity_act_func = DerivableFunction(identity, identity_deriv, 'identity')
+sigm_act_func = DerivableFunction(sigm, sigm_deriv, 'sigm')
 act_dict = {
-    'identity': identity_act_func
+    'identity': identity_act_func,
+    'sigm': sigm_act_func
 }
 
 
@@ -78,8 +88,8 @@ def std_weight_init(n_out, n_in, sparse_count=0):
 
 # Xavier init
 def norm_weight_init(n_out, n_in, sparse_count=0):
-    init_mat = np.random.uniform(low=-np.sqrt(6) / np.sqrt(n_in + n_out),
-                                 high=np.sqrt(6) / np.sqrt(n_in + n_out),
+    init_mat = np.random.uniform(low=-np.sqrt(6 / (n_in + n_out)),
+                                 high=np.sqrt(6 / (n_in + n_out)),
                                  size=(n_out, n_in))
 
     if 0 < sparse_count < n_in:

layer.py
@@ -13,7 +13,7 @@ Parameters:
 
 Attributes:
     -grad: gradient of error w.r.t. weights of the layer 
-    -in_val: input value for the layer
+    -in_val: input value for the layer (vector)
     -net: Dot product btw weights (matrix) and input (vector)
 """
 
@@ -27,14 +27,14 @@ class Layer:
         self.n_out = n_out
         self.bias = bias
         self.act_func = act_func
-        self.grad = None
+        self.grad = np.zeros((self.n_out, self.n_in))
         self.in_val = None
         self.net = None
 
         if init_func is None:
             self.weights = np.ones((n_out, n_in))  # Each row is composed of the weights of the unit
         else:
-            self.weights = init_func(n_out, n_in, 1)  # TODO add parameter for sparse count
+            self.weights = init_func(n_out, n_in, 0)  # TODO add parameter for sparse count
 
     """
         Computes layer forward pass
@@ -68,16 +68,19 @@ class Layer:
 
         # delta = deriv_err * f'(net_t)
         delta = np.multiply(deriv_err, self.act_func.deriv(self.net))
-        # grad = delta_i * output of previous layer (o_u)
-        self.grad = np.multiply(delta, self.in_val)
+        # grad += delta_i * output of previous layer (o_u)
+        self.grad += np.matmul(delta, np.transpose(self.in_val))
 
         return np.matmul(self.weights, delta)
 
     def __str__(self):
 
         cur_str = f"\tnumber units: {self.n_out}, number weights: {self.n_in}"
-
-        if self.grad is not None:
-            cur_str += f"\n\tlayer gradient vector: {self.grad}\n"
+        cur_str += f"\n\tlayer weights matrix: {self.weights}\n"
+        cur_str += f"\n\tlayer gradient vector: {self.grad}\n"
 
         return cur_str
+
+    def null_grad(self):
+
+        self.grad = np.zeros((self.n_out, self.n_in))

test/test.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('../')
 from network import Network
 from function import act_dict, init_dict, loss_dict
+from optimizer import GradientDescent
 
 
 def forward_test():
@@ -34,7 +35,14 @@ def backward_test():
     return bool_res
 
 
-print("Forward test:", forward_test())
-print("Backward test:", backward_test())
+# print("Forward test:", forward_test())
+# print("Backward test:", backward_test())
 
-
+net = Network(np.array([2, 2, 2]), init_dict["norm"], act_dict["sigm"], act_dict["identity"], loss_dict["squared"], 0.5)
+train_x = np.matrix('3 3; 3 3')
+train_y = np.matrix('6 6; 6 6')
+gd = GradientDescent(net, 0.1, 1, 500)
+gd.optimize(train_x, train_y)
+print("\n\n\n")
+print(net)
+print(net.forward(np.array([3, 3])))