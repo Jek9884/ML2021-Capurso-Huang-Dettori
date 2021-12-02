Fixed dimensions problem, added plot norm gradient, added average gradient for smooth curve, fixed nll loss

data_handler.py
@@ -14,8 +14,8 @@ def read_monk(path):
             elem = elem[1:]
             patterns.append(elem)
 
-        patterns = np.matrix(patterns, dtype=int)
-        targets = np.matrix(targets, dtype=int)
+        patterns = np.array(patterns, dtype=int)
+        targets = np.array(targets, dtype=int, ndmin=2)
         patterns = normalise_data(patterns)
 
         if targets.shape[0] == 1:

debug_tools.py
@@ -14,7 +14,10 @@ def plot_learning_curve(network, optimizer, train_x, train_y, tot_epochs, inter_
         if metric.name == "nll":
             out_vec = network.forward(train_x, net_out=True)
             metric_res = metric(train_y, out_vec)
-            metric_res = np.sum(metric_res)
+            metric_res = np.average(metric_res, axis=0)
+        elif metric.name == "squared":
+            pred_vec = network.forward(train_x)
+            metric_res = metric(train_y, pred_vec)
         else:
             pred_vec = network.forward(train_x)
             pred_vec[pred_vec < 0.5] = 0
@@ -24,7 +27,28 @@ def plot_learning_curve(network, optimizer, train_x, train_y, tot_epochs, inter_
 
         results_tr.append(metric_res)
 
+    results_tr = np.array(results_tr)
     plt.plot(range(0, tot_epochs, inter_epochs), results_tr)
     plt.xlabel("Epochs")
-    plt.ylabel("Metric")
+    plt.ylabel(f"Metric ({metric.name})")
+    plt.show()
+
+
+def plot_gradient_norm(network, optimizer, train_x, train_y, tot_epochs, inter_epochs, norm="fro"):
+
+    for i in range(tot_epochs//inter_epochs):
+
+        optimizer.train(network, train_x, train_y, epochs=inter_epochs)
+
+        for j, layer in enumerate(network.layers):
+
+            norm_grad_w = np.linalg.norm(layer.grad_w, ord=norm)
+            norm_grad_b = np.linalg.norm(layer.grad_w, ord=norm)
+
+            plt.scatter(i*inter_epochs, norm_grad_w, label=f"grad_w ({j})")
+            plt.scatter(i*inter_epochs, norm_grad_b, label=f"grad_b ({j})")
+
+    plt.legend()
+    plt.xlabel("Epochs")
+    plt.ylabel(f"Norm value ({norm})")
     plt.show()

functions/loss_funcs.py
@@ -13,7 +13,7 @@ def squared_loss_deriv(exp_val, pred_val):
 # Note: nll for now supports only a sigmoid output 
 # nll_loss assumes the network output net as input, the sigmoid is already factored in
 def nll_loss_bin(exp_val, pred_val_net):  # Negative log-likelihood
-    t1 = np.subtract(1, exp_val)
+    t1 = np.multiply(np.subtract(1, exp_val), pred_val_net)
     t2 = np.log(1+np.power(np.e, np.negative(pred_val_net)))
 
     return np.add(t1, t2)

hype_search.py
@@ -53,9 +53,21 @@ def grid_search(train_x, train_y, par_dict_net, par_dict_opt, k, metric):
     print(f"Number of tasks to execute: {len(list_tasks)}")
 
     # -2: Leave one core free
+    # This is a barrier for the parallel computation
     results = Parallel(n_jobs=-2, verbose=0)(list_tasks)
 
-    best_score = 0
+    return compare_results_metric(results, metric)
+
+
+def compare_results_metric(results, metric):
+
+    if metric.name in ["miscl. error", "nll"]:
+        best_score = np.inf
+        sign = -1
+    else:
+        best_score = 0
+        sign = 1
+
     best_combo = None
 
     # This is a barrier for the parallel computation
@@ -63,12 +75,17 @@ def grid_search(train_x, train_y, par_dict_net, par_dict_opt, k, metric):
 
         cur_val_score = result[1]
 
-        if best_score < cur_val_score:
+        if metric.name in ["nll"]:
+            cur_val_score = np.average(cur_val_score, axis=0)
+
+        c1 = best_score < cur_val_score and sign == 1
+        c2 = best_score > cur_val_score and sign == -1
+
+        if c1 or c2:
             best_score = cur_val_score
             best_combo = [result[2], result[3]]  # Return both dicts
 
     return best_score, best_combo
-
 
 def kfold_cv(par_combo_net, par_combo_opt, x_mat, y_mat, k, metric, seed=42):
 

optimizer.py
@@ -66,13 +66,15 @@ class GradientDescent:
         out = net.forward(sub_train_x)
         net.backward(sub_train_y, out)
 
-        self.__update_weights(net)
+        self.__update_weights(net, sub_train_x.shape[0])
 
-    def __update_weights(self, net):
+    def __update_weights(self, net, num_patt):
 
         for layer in net.layers:
-            delta_w = self.lr * layer.grad_w
-            delta_b = self.lr * layer.grad_b
+            avg_grad_w = np.divide(layer.grad_w, num_patt)
+            avg_grad_b = np.divide(layer.grad_b, num_patt)
+            delta_w = self.lr * avg_grad_w
+            delta_b = self.lr * avg_grad_b
 
             if self.momentum_val != 0:
                 delta_w += self.momentum_val * layer.delta_w_old

test.py
@@ -5,18 +5,18 @@ from network import Network
 from functions.loss_funcs import loss_dict
 from functions.act_funcs import act_dict
 from functions.init_funcs import init_dict
-from functions.metric_funcs import accuracy, miscl_error
+from functions.metric_funcs import metr_dict
 from optimizer import GradientDescent
 from data_handler import read_monk
 from hype_search import grid_search
-from debug_tools import plot_learning_curve
+from debug_tools import plot_learning_curve, plot_gradient_norm
 
 
 def forward_test():
-    in_vec = np.array([3, 3])
-    exp_res = np.array([12, 12])
+    in_vec = np.array([[3, 3]])
+    exp_res = np.array([[12, 12]])
 
-    net = Network(np.array([2, 2, 2]), None, act_dict["identity"],
+    net = Network([2, 2, 2], None, act_dict["identity"],
                   act_dict["identity"], loss_dict["squared"])
 
     out = net.forward(in_vec)
@@ -25,10 +25,10 @@ def forward_test():
 
 
 def backward_test():
-    in_vec = np.array([3, 3])
-    exp_res = np.array([6, 6])
+    in_vec = np.array([[3, 3]])
+    exp_res = np.array([[6, 6]])
 
-    net = Network(np.array([2, 2, 2]), None, act_dict["identity"],
+    net = Network([2, 2, 2], None, act_dict["identity"],
                   act_dict["identity"], loss_dict["squared"])
 
     out = net.forward(in_vec)
@@ -48,56 +48,60 @@ def backward_test():
 
 
 def simple_learning_test_regression():
-    in_vec = np.array([3, 3])
-    exp_res = np.array([6, 6])
+    train_x = np.array([3, 3])
+    train_y = np.array([6, 6])
 
-    net = Network(np.array([2, 2, 2]), init_dict["norm"], act_dict["sigm"],
+    net = Network([2, 2, 2], init_dict["norm"], act_dict["sigm"],
                   act_dict["identity"], loss_dict["squared"])
 
-    gd = GradientDescent(0.1, -1, epochs=20)
-    gd.train(net, np.asmatrix(in_vec), np.asmatrix(exp_res))
+    gd = GradientDescent(0.5, -1, epochs=20)
+    gd.train(net, train_x, train_y)
 
-    return net.forward(in_vec)
+#    plot_learning_curve(net, gd, train_x, train_y, 100, 1, loss_dict["squared"])
+    return net.forward(train_x)
 
 
 def simple_and_learning_test_classification():  # Func: A and B
-    train_x = np.matrix('0 0; 0 1; 1 0; 1 1')
-    train_y = np.matrix('0; 0; 0; 1')
+    train_x = np.asarray(np.matrix('0 0; 0 1; 1 0; 1 1'))
+    train_y = np.asarray(np.matrix('0; 0; 0; 1'))
 
-    net = Network(np.array([2, 1]),
+    net = Network([2, 1],
                   init_dict["norm"],
                   act_dict["tanh"],
                   act_dict["sigm"],
                   loss_dict["nll"])
 
-    gd = GradientDescent(1, -1, epochs=100)
+    gd = GradientDescent(0.5, -1, epochs=100)
     gd.train(net, train_x, train_y)
 
     net_pred = net.forward(train_x)
     net_pred[net_pred < 0.5] = 0
     net_pred[net_pred >= 0.5] = 1
 
-    return accuracy(train_y, net_pred)
+#    plot_learning_curve(net, gd, train_x, train_y, 100, 1, loss_dict["nll"])
+    return metr_dict["accuracy"](train_y, net_pred)
 
 
 def simple_learning_test_classification():  # Func: (A or B) xor (C or D)
-    train_x = np.matrix('0 0 0 0; 0 1 0 1; 0 0 0 1; 0 1 0 0; 1 0 0 0; 1 0 1 0; 1 1 1 1')
-    train_y = np.matrix('0; 0; 1; 1; 1; 0; 0')
+    train_x = np.asarray(
+        np.matrix('0 0 0 0; 0 1 0 1; 0 0 0 1; 0 1 0 0; 1 0 0 0; 1 0 1 0; 1 1 1 1'))
+    train_y = np.asarray(np.matrix('0; 0; 1; 1; 1; 0; 0'))
 
-    net = Network(np.array([4, 4, 1]),
+    net = Network([4, 4, 1],
                   init_dict["norm"],
                   act_dict["tanh"],
                   act_dict["sigm"],
                   loss_dict["nll"])
 
-    gd = GradientDescent(0.1, -1, epochs=100)
+    gd = GradientDescent(0.5, -1, epochs=100)
     gd.train(net, train_x, train_y)
 
     net_pred = net.forward(train_x)
     net_pred[net_pred < 0.5] = 0
     net_pred[net_pred >= 0.5] = 1
 
-    return accuracy(train_y, net_pred)
+#    plot_learning_curve(net, gd, train_x, train_y, 500, 1, loss_dict["nll"])
+    return metr_dict["accuracy"](train_y, net_pred)
 
 
 def test_monk(path_train, path_test):
@@ -115,22 +119,24 @@ def test_monk(path_train, path_test):
 
     dict_param_sgd = {
         'lr': [0.01, 0.2, 0.5, 0.8],
-        'batch_size': [-1],
+        'batch_size': [1],
         'reg_val': [0, 0.001, 0.01, 0.1,],
         'reg_type': [2],
         'momentum_val': [0, 0.01, 0.1],
         'nesterov': [False, True],
-        'epochs': [20]
+        'epochs': [20, 100]
     }
 
+    metric = loss_dict["nll"]
     best_result, best_combo = grid_search(train_x, train_y,
-                                          dict_param_net, dict_param_sgd, 10, accuracy)
-    print("Best accuracy score (train): ", best_result)
+                                          dict_param_net, dict_param_sgd, 10, metric)
+    print(f"Best {metric.name} score (train): ", best_result)
     print(best_combo)
 
     net = Network(**best_combo[0])
     gd = GradientDescent(**best_combo[1])
-    plot_learning_curve(net, gd, train_x, train_y, 100, 1, loss_dict["nll"])
+#    plot_gradient_norm(net, gd, train_x, train_y, 100, 20)
+    plot_learning_curve(net, gd, train_x, train_y, 100, 5, metric)
 
     return best_result
 