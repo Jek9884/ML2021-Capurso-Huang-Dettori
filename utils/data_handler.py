2eea84b164256477b6d5b673c34cbfcac0103bb2
Antonello Dettori <dev@dettori.io>
2021-12-18 05:04:07 +0100

    data_handler: added 1-hot encoding

--- utils/data_handler.py
+++ utils/data_handler.py
@@ -15,6 +15,8 @@ def read_monk(path, norm_data=True):
             patterns.append(elem)
 
         patterns = np.array(patterns, dtype=int)
+        patterns = one_hot_encode(patterns)
+
         targets = np.array(targets, dtype=int, ndmin=2)
 
         if norm_data:
@@ -24,6 +26,29 @@ def read_monk(path, norm_data=True):
             targets = targets.transpose()
 
     return patterns, targets
+
+
+def one_hot_encode(patterns):
+
+    final_patt = None
+
+    for i in range(patterns.shape[1]):
+
+        cur_col = patterns[:, i]
+        unique_vals = np.unique(cur_col)
+        num_values_var = len(unique_vals)
+        feat_mat = np.zeros((patterns.shape[0], num_values_var))
+
+        for val in unique_vals:
+            #Note: could be a dangerous approach
+            feat_mat[cur_col == val, val-1] = 1
+
+        if final_patt is None:
+            final_patt = feat_mat
+        else:
+            final_patt = np.concatenate((final_patt, feat_mat), axis=1)
+
+    return final_patt
 
 
 def normalise_data(patterns):
